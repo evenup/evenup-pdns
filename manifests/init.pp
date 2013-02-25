02626abf04c201a1f8e8a611ef13f275d0e304f5
Justin Lambert <jlambert@eml.cc>
2013-02-25 16:16:31 -0700

    moved to parameterized class

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,20 +8,20 @@
 # === Examples
 #
 # * Installation:
-#     class { 'apache': }
+#     class { 'pdns': }
 #
 #
 # === Authors
 #
 # * Justin Lambert <mailto:jlambert@letsevenup.com>
 #
-class pdns {
-
-  $puppetdb_server = hiera('pdns::puppetdb_server', 'puppet')
-  $puppetdb_logfile = hiera('pdns::puppetdb_logfile', '/var/log/pdns_puppetdb.log')
-  $puppetdb_loglevel = hiera('pdns::puppetdb_loglevel', 'info')
-  $puppetdb_reload = hiera('pdns::puppetdb_reload', '30')
-  $pdns_regex = hiera('pdns::pdns_regex', '')
+class pdns(
+  $puppetdb_server    = 'puppet',
+  $puppetdb_logfile   = '/var/log/pdns_puppetdb.log',
+  $puppetdb_loglevel  = 'info',
+  $puppetdb_reload    = '30',
+  $pdns_regex         = ''
+) {
 
   class {'pdns::install': } ->
   class {'pdns::config': } ->
