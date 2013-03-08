bdf105acf03823b34dd6a26d004fdeeaccc82286
Justin Lambert <jlambert@eml.cc>
2013-03-07 21:17:23 -0700

    add monitoring through sensu

--- manifests/service.pp
+++ manifests/service.pp
@@ -23,6 +23,24 @@ class pdns::service {
   $monitoring = hiera('monitoring', '')
 
   case $monitoring {
-    'sensu':  { sensu::client_subscription { 'pdns': } }
+    'sensu': {
+      sensu::check { 'pdns-recursor-running':
+        handlers    => ['default'],
+        standalone  => true,
+        subscribers => ['pdns'],
+        command     => '/etc/sensu/plugins/check-procs.rb -p /usr/sbin/pdns_recursor -c 1 -C 1',
+        refresh     => 1800,
+      }
+
+      sensu::check { 'pdns-server-running':
+        handlers    => 'default',
+        standalone  => true,
+        subscribers => ['pdns'],
+        command     => '/etc/sensu/plugins/check-procs.rb -p /usr/sbin/pdns_server -w 2 -c 2 -W 2 -C 1',
+        refresh     => 1800,
+      }
+
+      sensu::subscription { 'pdns': }
+    }
   }
 }
