b4e41a47a481a2a3829128d45ece65c385500447
Justin Lambert <jlambert@eml.cc>
2013-03-08 05:04:36 -0700

    break monitoring out to new class

--- README.markdown
+++ README.markdown
@@ -52,6 +52,11 @@ Dependencies:
 * [evenup-ruby](https://github.com/evenup/evenup-ruby)
 
 
+Monitoring:
+-------------
+Monitoring is controled through the hiera variable 'monitoring'.  Currently there is support
+for [Sensu](http://sensuapp.com).  Please feel free to add other monitoring tools.
+
 Known Issues:
 -------------
 * Only tested on RHEL 6.3 and puppet 3.0.1 using the pdns packages from EPEL
--- manifests/service.pp
+++ manifests/service.pp
@@ -24,23 +24,7 @@ class pdns::service {
 
   case $monitoring {
     'sensu': {
-      sensu::check { 'pdns-recursor-running':
-        handlers    => ['default'],
-        standalone  => true,
-        subscribers => ['pdns'],
-        command     => '/etc/sensu/plugins/check-procs.rb -p /usr/sbin/pdns_recursor -c 1 -C 1',
-        refresh     => 1800,
-      }
-
-      sensu::check { 'pdns-server-running':
-        handlers    => 'default',
-        standalone  => true,
-        subscribers => ['pdns'],
-        command     => '/etc/sensu/plugins/check-procs.rb -p /usr/sbin/pdns_server -w 2 -c 2 -W 2 -C 1',
-        refresh     => 1800,
-      }
-
-      sensu::subscription { 'pdns': }
+      include pdns::monitoring::sensu
     }
   }
 }
